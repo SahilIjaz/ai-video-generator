1cdfd3025996aff4ad07e497cf3bc6b0531c42fb
SahilIjaz <hssahil2913@gmail.com>
2025-12-02 01:03:13 +0500

    project initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Helo !</h1>
+      <Button variant={"ghost"}>Subscribe</Button>
+    </div>
+  );
+}
